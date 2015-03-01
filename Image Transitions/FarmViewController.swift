92c9037aa6fff52bc5d7cb8099d1f24b647ebee5
scotttong <scotttong@sockmonster-2.local>
2015-03-01 00:37:06 -0800

    Fade transition
    override the standard modal segue and replace it with a fade transition

--- Image Transitions/FarmViewController.swift
+++ Image Transitions/FarmViewController.swift
@@ -17,6 +17,9 @@ class FarmViewController: UIViewController {
 	// 8. set a class variable to capture the image that was tapped on
 	var selectedImageView: UIImageView!
 	
+	// 12. create a new variable that holds the special transition
+	var fadeTransition: FadeTransition!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -65,8 +68,20 @@ class FarmViewController: UIViewController {
 		
 		// 7. in order to do pass the image you tapped on in this view controller to the destinationViewController, we have to get that image from from the tap gesture recognizer. Before we do that though, we want to create a class variable at the top called "selectedImageView" which we'll use to capture it, store it, then use it here to access it, and pass it off to the destinationViewController.
 		
-		// 10. set the image on the destinationViewController to the image that was tapped on in this view controller.
+		// 10. set the image on the destinationViewController to the image that was tapped on in this view controller. At this point, the file should pass both the text from the textField and selectedImage over to the placeholders on the toViewController.
+		
 		destinationViewController.petImage = selectedImageView.image
+		
+		// 11. next, we need to add .swift files created by Tim called "BaseTransition.swift" and "FadeTransition.swift" to our XCode project to help us with animating the transition. Step 12 is above near the outlets.
+		
+		// 13. this is where we take over the standard transition (initialize the transition, set the presentation style to custom, and set the delegate to this new instance of the fade transition. I'm going to comment this out because we want to do a more complicated transition.
+		
+		fadeTransition = FadeTransition()
+		destinationViewController.modalPresentationStyle = UIModalPresentationStyle.Custom
+		destinationViewController.transitioningDelegate = fadeTransition
+		
+		
+		
 		
 		// Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
